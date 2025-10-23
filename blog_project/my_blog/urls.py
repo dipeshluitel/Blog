5820e9a7c5f891f9e2c50980eb5d071680f55ec0
Dipesh <dipeshluitel40@gmail.com>
2025-10-23 20:09:28 +0545

    UpdateView and DeleteView has been created mapped to urls.py(app)

--- blog_project/my_blog/urls.py
+++ blog_project/my_blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('about/',views.PostListView.as_view(),name='about'),
     path('post/<int:pk>/',views.PostDetailView.as_view(),name='post_detail'),
     path('post/new/',views.CreatePostView.as_view(),name='post_new'),
+    path('post/<int:pk>/edit/',views.PostUpdateView.as_view(),name='post_new'),
+    path('post/<int:pk>/delete/',views.PostDeleteView.as_view(),name='post_new'),
 ]
