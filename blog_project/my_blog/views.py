36f1ed1151e023b5829aea9f8008d3472509e979
Dipesh <dipeshluitel40@gmail.com>
2025-10-24 18:41:26 +0545

    function view created to add comments

--- blog_project/my_blog/views.py
+++ blog_project/my_blog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
 from django.utils import timezone
 from my_blog.forms import PostForm, CommentForm
 from my_blog.models import Post,Comment
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin  # in place of decorators for CBV
+from django.contrib.auth.decorators import login_required
 from django.views.generic import (TemplateView, ListView,DetailView,CreateView,UpdateView,DeleteView)
 # Create your views here.
 
@@ -44,3 +45,23 @@ class DraftListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+# #############
+###############
+####
+#
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post,pk = pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+        
+        else:
+            form = CommentForm()
+        return render(request, 'my_blog/comment_form.html', {'form':form})
